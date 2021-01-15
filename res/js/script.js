41baca605c15edaaba27e21b05b2682d49edc29f
Faizal Chan <moefc32@gmail.com>
2021-01-16 03:54:38 +0700

    update

--- res/js/script.js
+++ res/js/script.js
@@ -3,8 +3,30 @@ window.onscroll = function () {
 };
 
 function scrollIndicator() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let scrolled = (winScroll / height) * 100;
   document.getElementById("bar").style.width = scrolled + "%";
 }
+
+const apiKey = 'EEFc6yBDOHDISwhWKGmulhPCb7ETWkav';
+const user = 'moefc32';
+
+$(function () {
+  $.ajax({
+    url: "https://api.behance.net/v2/users/" + user + "/projects?api_key=" + apiKey,
+    dataType: 'jsonp',
+    success: function (results) {
+      let projectsArray = new Array();
+      for (let i = 0; i < results.projects.length; i++) {
+        let project = results.projects[i];
+
+        let output;
+        output = '<div class="col-xs-12 col-sm-6 col-md-4"><a href="' + project.url + '"><div class="project-container"><div class="img-backdrop"></div><div class="description-container"><h2 class="caption">' + project.name + '</h2></div><img src="' + project.covers[404] + '" class="img-responsive"></div></a></div>';
+        projectsArray.push(output);
+      }
+      let $content = $('#behance-feed');
+      $content.html(projectsArray);
+    }
+  });
+});
